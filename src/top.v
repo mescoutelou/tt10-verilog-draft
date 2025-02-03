b58940abe1ca53c828c56d1c43be9c4f4efcbc08
Mathieu Escouteloup <mathieu.escouteloup@enseirb-matmeca.fr>
2025-02-03 16:45:11 +0100

    Restore original design

--- src/top.v
+++ src/top.v
@@ -17,12 +17,13 @@ module tt_um_draft (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-//  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uo_out = {4'b0101, 4'b0000};
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+//  assign uo_out = {4'b0101, 4'b0000};
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, ui_in, uio_in, 1'b0};
+//  wire _unused = &{ena, clk, rst_n, ui_in, uio_in, 1'b0};
+  wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
