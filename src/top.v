9f92645d870c956414a73e525216d1c475564ef2
Mathieu Escouteloup <mathieu.escouteloup@enseirb-matmeca.fr>
2025-02-03 16:34:39 +0100

    Add unused signals

--- src/top.v
+++ src/top.v
@@ -23,6 +23,6 @@ module tt_um_draft (
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, ui_in, uio_in, 1'b0};
 
 endmodule
